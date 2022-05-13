dfeda98eca63b19611b7314b5b1a204e48a34d21
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-05-13 22:13:49 +0200

    added logic to top section of footer(all links)

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -11,35 +11,16 @@
                                 <li>
                                     <h3>DC COMICS</h3>
                                 </li>
-                                <li>
-                                    <a href="#">Characters</a>
-                                </li>
-                                <li>
-                                    <a href="#">Comics</a>
-                                </li>
-                                <li>
-                                    <a href="#">Movies</a>
-                                </li>
-                                <li>
-                                    <a href="#">TV</a>
-                                </li>
-                                <li>
-                                    <a href="#">Games</a>
-                                </li>
-                                <li>
-                                    <a href="#">Videos</a>
-                                </li>
-                                <li>
-                                    <a href="#">News</a>
+                                <li v-for="(link, index)  in DCComicsArr"
+                                :key="`dc-comics${index}`">
+                                    <a href="link.href">{{link.link}}</a>
                                 </li>
                                 <li>
                                     <h3>SHOP</h3>
                                 </li>
-                                <li>
-                                    <a href="#">Shop DC</a>
-                                </li>
-                                <li>
-                                    <a href="#">Shop DC Collectibles</a>
+                                <li v-for="(link, index)  in shopArr"
+                                :key="`shop${index}`">
+                                    <a href="link.href">{{link.link}}</a>
                                 </li>
                             </ul>
                         </div>
@@ -48,38 +29,9 @@
                                 <li>
                                     <h3>DC</h3>
                                 </li>
-                                <li>
-                                    <a href="#">Terms of Use</a>
-                                </li>
-                                <li>
-                                    <a href="#">Privacy policy(New)</a>
-                                </li>
-                                <li>
-                                    <a href="#">Ad Choices</a>
-                                </li>
-                                <li>
-                                    <a href="#">Advertising</a>
-                                </li>
-                                <li>
-                                    <a href="#">Jobs</a>
-                                </li>
-                                <li>
-                                    <a href="#">Subscriptions</a>
-                                </li>
-                                <li>
-                                    <a href="#">Talent Workshops</a>
-                                </li>
-                                <li>
-                                    <a href="#">CPSC Certificates</a>
-                                </li>
-                                <li>
-                                    <a href="#">Ratings</a>
-                                </li>
-                                <li>
-                                    <a href="#">Shop Help</a>
-                                </li>
-                                <li>
-                                    <a href="#">Contact Us</a>
+                                <li v-for="(link, index)  in DCArr"
+                                :key="`DC${index}`">
+                                    <a href="link.href">{{link.link}}</a>
                                 </li>
                             </ul>
                         </div>
@@ -88,20 +40,9 @@
                                 <li>
                                     <h3>SITES</h3>
                                 </li>
-                                <li>
-                                    <a href="#">DC</a>
-                                </li>
-                                <li>
-                                    <a href="#">MAD Magazine</a>
-                                </li>
-                                <li>
-                                    <a href="#">DC Kids</a>
-                                </li>
-                                <li>
-                                    <a href="#">DC Universe</a>
-                                </li>
-                                <li>
-                                    <a href="#">DC Power Visa</a>
+                                <li v-for="(link, index)  in sitesArr"
+                                :key="`sites-arr${index}`">
+                                    <a href="link.href">{{link.link}}</a>
                                 </li>
                             </ul>
                         </div>
@@ -135,7 +76,122 @@
 
 <script>
 export default {
-    name: "FooterComponent"
+    name: "FooterComponent",
+    data(){
+        return{
+            DCComicsArr: [
+                {
+                    link: "Characters",
+                    href: "#"
+                },
+                {
+                    link: "Comics",
+                    href: "#"
+                },
+                {
+                    link: "Movies",
+                    href: "#"
+                },
+                {
+                    link: "TV",
+                    href: "#"
+                },
+                {
+                    link: "Games",
+                    href: "#"
+                },
+                {
+                    link: "Videos",
+                    href: "#"
+                },
+                {
+                    link: "News",
+                    href: "#"
+                }
+            ],
+
+            shopArr: [
+                {
+                    link:"Shop DC",
+                    href: "#"
+                },
+                {
+                    link:"Shop DC Collectibles",
+                    href: "#"
+                }
+            ],
+
+            DCArr: [
+                {
+                    link:"Terms of Use",
+                    href: "#"
+                },
+                {
+                    link:"Privacy policy(New)",
+                    href: "#"
+                },
+                {
+                    link:"Ad Choices",
+                    href: "#"
+                },
+                {
+                    link:"Advertising",
+                    href: "#"
+                },
+                {
+                    link:"Jobs",
+                    href: "#"
+                },
+                {
+                    link:"Subscriptions",
+                    href: "#"
+                },
+                {
+                    link:"Talent Workshops",
+                    href: "#"
+                },
+                {
+                    link:"CPSC Certificates",
+                    href: "#"
+                },
+                {
+                    link:"Ratings",
+                    href: "#"
+                },
+                {
+                    link:"Shop Help",
+                    href: "#"
+                },
+                {
+                    link:"Contact Us",
+                    href: "#"
+                }
+            ],
+
+            sitesArr: [
+                {
+                    link: "DC",
+                    href: "#"
+                },
+                {
+                    link: "MAD Magazine",
+                    href: "#"
+                },
+                {
+                    link: "DC Kids",
+                    href: "#"
+                },
+                {
+                    link: "DC Universe",
+                    href: "#"
+                },
+                {
+                    link: "DC Power Visa",
+                    href: "#"
+                }
+            ]
+        }
+    }
 }
 </script>
 
